ffd8478d2dfca8cfe6befe871ca7a50de9ab66ba
SeungBeomHu <hsb0423@naver.com>
2023-11-13 12:15:26 +0900

    fix: Set method 인터페이스화(member func -> virtual function으로 변경 및 .cpp 파일 제거)

--- cse_set.h
+++ cse_set.h
@@ -4,18 +4,14 @@
 
 class Set {
 public:
-    bool empty();
-    int size();
-    int erase(int key);
-    int insert(int key);
-    int find(int key);
-    std::pair<int,int> minimum(int key);
-    std::pair<int,int> maximum(int key);
-    int rank(int key);
-
-private:
-
-
+    virtual bool empty()=0;
+    virtual int size()=0;
+    virtual int erase(int key)=0;
+    virtual int insert(int key)=0;
+    virtual int find(int key)=0;
+    virtual std::pair<int,int> minimum(int key)=0;
+    virtual std::pair<int,int> maximum(int key)=0;
+    virtual int rank(int key)=0;
 };
 
 #endif AVLTREE_CSE_SET_H
