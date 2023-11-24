b5c5ae0f923add3986d97c3293d7626f5d135cfe
suyong <edgarlee9364@gmail.com>
2023-11-24 14:52:59 +0900

    Insert 테스트 케이스 구현

---
+++ test/insertTest.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include "avl_node.h"
+#include "avl_tree.h"
+#include "node.h"
+#include "tree_node.h"
+#include "cse_set.h"
+using namespace std;
+
+int main(){
+    Set* avltree = new AVLTree();
+
+    vector<int> keys = {30, 20, 40, 10, 25, 50, 22};
+
+    for(int key : keys){
+        cout << "Insert: " << key << "\n";
+        avltree.insert(key);
+        cout << "\n";
+    }
+    return 0;
+
+}
