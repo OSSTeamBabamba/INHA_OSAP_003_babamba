8330d5ae13f8622d64ba2ecb2dd7cfba82174979
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-30 16:52:13 +0900

    feat: 노드에 사이즈를 저장하는 멤버변수 추가
    기존에는 노드에는 키값만 저장되어 있었다면 현재는 해당 노드를 루트로 하는 서브트리의 사이즈도 저장할 수 있게 멤버변수 추가

--- node.h
+++ node.h
@@ -1,16 +1,20 @@
 #ifndef AVLTREESTL_NODE_H
 #define AVLTREESTL_NODE_H
-#include<cstdint>
+#include <cstdint>
 
-class Node{
+class Node {
 public:
- Node(int32_t key): key_(key){}
+    Node(int32_t key) : key_(key), size_(1) {}
 
     int32_t key() const;
     void setKey(int32_t key);
 
+    int32_t size() const;
+    void setSize(int32_t size);
+
 protected:
     int32_t key_;
+    int32_t size_;
 };
 
 #endif //AVLTREESTL_NODE_H
