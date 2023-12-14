02451a6229a2c2f913af951eea3bcdbb800ab3cf
tjdgns8439 <tjdgns8439@inha.edu>
2023-12-14 21:14:59 +0900

    feat: Minimum, Rank Test
    - Minimum, Rank에 대한 google test 진행

--- tests/insertTest.cpp
+++ tests/insertTest.cpp
@@ -1,5 +1,4 @@
-﻿#include<iostream>
-#include<algorithm>
+﻿#include<algorithm>
 #include<vector>
 #include<utility>
 #include "../avl_tree.h"
@@ -23,6 +22,130 @@ TEST(SET_AVLTEST,InsertTest2){
     }
 }
 
+
+//값을 하나만 Insert 후 Minumum의 키
+TEST(SET_AVLTEST, FindMinimumKey0) {
+    Set* avlTree = new AVLTree();
+    avlTree->Insert(1);  // 1 삽입
+
+    // Test the Minimum function
+    EXPECT_EQ(avlTree->Minimum(1).first, 0);  // 크기가 1인 트리의 Mimimum의 key는 트리에 포함된 유일한 값인 루트
+}
+
+//여러 값 Insert 후 Minumum의 키
+TEST(SET_AVLTEST, FindMinimumKey1) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);  // AVL트리에 keys 삽입
+
+    // Test the Minimum function
+    EXPECT_EQ(avlTree->Minimum(10).second, 3);  // 10을 루트로 하는 서브트리의 key는 3
+    EXPECT_EQ(avlTree->Minimum(5).second, 3);    // 5를 루트로 하는 서브트리의 key는 3
+    EXPECT_EQ(avlTree->Minimum(15).second, 12);  // 15을 루트로 하는 서브트리의 key는 12
+    EXPECT_EQ(avlTree->Minimum(7).second, 7);  // 7을 루트로 하는 서브트리의 key는 7
+    ASSERT_EQ(avlTree->Minimum(20).first, -1);   // 20은 해당 트리에 존재하지 않기에 -1 반환
+}
+
+//Insert 후 Minimum의 Depth
+TEST(SET_AVLTEST, FindMinimumDepth) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7};
+    //AVL트리에 keys 삽입
+    for(auto key : keys) avlTree->Insert(key);
+
+    // Test the Minimum function
+    EXPECT_EQ(avlTree->Minimum(10).first, 2);  // 10을 루트로 하는 서브트리의 depth는 2
+    EXPECT_EQ(avlTree->Minimum(5).first, 2);  // 5를 루트로 하는 서브트리의 depth는 2
+    EXPECT_EQ(avlTree->Minimum(15).first, 1);  // 15를 루트로 하는 서브트리의 depth는 1
+    ASSERT_EQ(avlTree->Minimum(20).first, -1); // 20은 해당 트리에 존재하지 않기에 -1 반환
+}
+
+//Insert 후 Minimum Node
+TEST(SET_AVLTEST, FindMinimumNode) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7};
+    //AVL트리에 keys 삽입
+    for(auto key : keys) avlTree->Insert(key);
+
+    // Test the Minimum function
+    EXPECT_EQ(avlTree->Minimum(10), make_pair(2,3));  // 10을 루트로 하는 서브트리의 Node는 (depth, Key)는 (2,3)
+    EXPECT_EQ(avlTree->Minimum(5), make_pair(2,3));  // 5를 루트로 하는 서브트리의 Node는 (depth, Key)는 (2,3)
+    EXPECT_EQ(avlTree->Minimum(15), make_pair(1,15));  // 15를 루트로 하는 서브트리의 Node는 (depth, Key)는 (2,12)
+    EXPECT_EQ(avlTree->Minimum(3), make_pair(2,3));  // 3를 루트로 하는 서브트리의 Node는 (depth, Key)는 (2,3)
+    EXPECT_EQ(avlTree->Minimum(7), make_pair(2,7));  // 7를 루트로 하는 서브트리의 Node는 (depth, Key)는 (2,7)
+    ASSERT_EQ(avlTree->Minimum(20), make_pair(-1,-1)); // 20은 해당 트리에 존재하지 않기에 (-1,-1)이 반환
+}
+
+
+//삽입 및 삭제 연산 후 Minimum 노드
+TEST(SET_AVLTEST, FindMinimumAfterErase) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);  // AVL트리에 keys 삽입
+
+    // Test the Minimum function
+    EXPECT_EQ(avlTree->Minimum(3), make_pair(2,3));  // 3을 루트로 하는 서브트리의 (depth, Key)는 (2,3)
+    EXPECT_EQ(avlTree->Minimum(10), make_pair(2,3));  // 10를 루트로 하는 서브트리의 (depth, Key)는 (2,3)
+    EXPECT_EQ(avlTree->Minimum(15), make_pair(2,12));  // 15를 루트로 하는 서브트리의 (depth, Key)는 (2,12)
+
+    avlTree->Erase(3); // AVL 트리에서 3을 키로 가진 노드 삭제
+    EXPECT_EQ(avlTree->Minimum(10), make_pair(1,5)); //10을 루트로 하는 서브트리의 Minumum노드는 3에서 5로 변경
+    ASSERT_EQ(avlTree->Minimum(3), make_pair(-1,-1)); //삭제된 3은 찾을 수 없다.
+}
+
+//Insert 후 Rank
+TEST(SET_AVLTEST, FindRank) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);
+
+    // Test the Rank function
+    EXPECT_EQ(avlTree->Rank(10).second, 4);  // 10의 랭크는 4
+    EXPECT_EQ(avlTree->Rank(3).second, 1);   // 3의 랭크는 1
+    EXPECT_EQ(avlTree->Rank(18).second, 7);  // 18의 랭크는 7
+    ASSERT_EQ(avlTree->Rank(20).first, -1); // 20은 해당 트리에 존재하지 않기에 -1 반환
+}
+
+//Insert 후 해당 노드의 깊이
+TEST(SET_AVLTEST, FindRankDepth) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);
+
+    // Test the Rank function
+    EXPECT_EQ(avlTree->Rank(10).first, 0);  // 10의 depth는 0
+    EXPECT_EQ(avlTree->Rank(3).first, 2);   // 3의 depth는 2
+    EXPECT_EQ(avlTree->Rank(18).first, 2);  // 18의 depth는 2
+    ASSERT_EQ(avlTree->Rank(20).first, -1); // 20은 해당 트리에 존재하지 않기에 -1 반환
+}
+
+TEST(SET_AVLTEST, FindRankNode) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);  //AVL트리에 keys 삽입
+
+    // Test the Rank function
+    EXPECT_EQ(avlTree->Rank(10), make_pair(0,4));  // 10의 (depth, rank)는 (0, 4)
+    EXPECT_EQ(avlTree->Rank(3), make_pair(2,1)); // 3의 (depth, rank)는 (2, 1)
+    EXPECT_EQ(avlTree->Rank(18), make_pair(2,7)); // 18의 (depth, rank)는 (2, 7)
+    ASSERT_EQ(avlTree->Rank(20).first, -1); // 20은 해당 트리에 존재하지 않기에 -1반환
+}
+
+//삽입 및 삭제 연산 후 Rank
+TEST(SET_AVLTEST, FindRankNodeAfterErase) {
+    Set* avlTree = new AVLTree();
+    vector<int> keys = {10,5,15,3,7,12,18};
+    for(auto key : keys) avlTree->Insert(key);
+
+    // Test the Rank function
+    EXPECT_EQ(avlTree->Rank(10).second, 4);  // 10의 랭크는 4
+    EXPECT_EQ(avlTree->Rank(3).second, 1);   // 3의 랭크는 1
+    EXPECT_EQ(avlTree->Rank(18).second, 7);  // 18의 랭크는 7
+
+    avlTree->Erase(3); // AVL 트리에서 3을 키로 가진 노드 삭제
+    EXPECT_EQ(avlTree->Rank(10).second, 3); //10을 루트로 하는 서브트리의 Rank 4->3으로 변경
+    ASSERT_EQ(avlTree->Rank(3).second, 0); //삭제된 3은 찾을 수 없다.
+}
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
